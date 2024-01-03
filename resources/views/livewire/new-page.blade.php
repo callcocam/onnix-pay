978de61537c75f1a717cc5e313727c7df72cda3f
Claudio Campos <callcocam@gmail.com>
2024-01-03 06:42:00 -0300

    traduzindo

--- resources/views/livewire/new-page.blade.php
+++ resources/views/livewire/new-page.blade.php
@@ -23,41 +23,41 @@
             </div>
         </x-content>
     </x-slot>
-    <div class="md:mx-auto flex w-full flex-col items-center justify-center md:max-w-7xl">
+    <div class="flex w-full flex-col items-center justify-center md:mx-auto md:max-w-7xl">
         <div class="my-16 flex flex-col">
             <div class="relative mx-auto mt-5 flex w-full flex-col items-center md:gap-2">
                 <div class="flex w-full flex-col items-center md:items-start">
-                    <h2 class="text-2xl text-yellow-500">Need to know about</h2>
-                    <h3 class="mb-4 text-4xl md:text-6xl font-semibold">HOW TO PLAY</h3>
-                    <p class="mb-10 text-sm">Follow these 3 easy steps!</p>
+                    <h2 class="text-2xl text-yellow-500">Precisa saber sobre</h2>
+                    <h3 class="mb-4 text-4xl font-semibold md:text-6xl">COMO JOGAR</h3>
+                    <p class="mb-10 text-sm">Siga essas 3 etapas!</p>
                 </div>
                 <div class="flex w-full flex-col md:flex-row">
-                    <div class="grid w-full max-w-[90%] md:max-w-full  grid-cols-1 justify-stretch gap-y-3 md:gap-3  md:w-4/6 md:grid-cols-3  mx-auto">
-                        <div class="bg-gradient-to-r from-violet-600 via-violet-600 to-indigo-600 flex min-h-80 flex-col items-center space-y-5 rounded-md p-2 py-5">
+                    <div class="mx-auto grid w-full max-w-[90%] grid-cols-1 justify-stretch gap-y-3 md:w-4/6 md:max-w-full md:grid-cols-3 md:gap-3">
+                        <div class="flex min-h-80 flex-col items-center space-y-5 rounded-md bg-gradient-to-r from-violet-600 via-violet-600 to-indigo-600 p-2 py-5">
                             <div class="relative mx-auto flex h-36 w-36 items-center justify-center rounded-full border">
                                 <img src="https://pixner.net/rifa1/demo/assets/images/icon/play/1.png" alt="" class="rounded-full p-6 shadow-inner shadow-purple-600 hover:shadow-lg" />
                                 <span class="absolute bottom-5 right-0 flex h-8 w-8 items-center justify-center rounded-full bg-purple-500 text-lg font-bold text-white opacity-85">01</span>
                             </div>
                             <div class="flex flex-col items-center text-center text-white">
-                                <span>CHOSE</span>
-                                <p class="px-5">Register to RIFA & Choose your contest</p>
+                                <span>SELECIONE</span>
+                                <p class="px-5">ESCOLHER Registre-se na RIFA e escolha seu concurso</p>
                             </div>
                         </div>
-                        <div class="bg-gradient-to-r from-violet-600 via-violet-600 to-indigo-600 flex min-h-80 flex-col items-center space-y-5 rounded-md p-2 py-5">
+                        <div class="flex min-h-80 flex-col items-center space-y-5 rounded-md bg-gradient-to-r from-violet-600 via-violet-600 to-indigo-600 p-2 py-5">
                             <div class="relative mx-auto flex h-36 w-36 items-center justify-center rounded-full border">
                                 <img src="https://pixner.net/rifa1/demo/assets/images/icon/play/2.png" alt="" class="rounded-full p-6 shadow-inner shadow-purple-600 hover:shadow-lg" />
                                 <span class="absolute bottom-5 right-0 flex h-8 w-8 items-center justify-center rounded-full bg-purple-500 text-lg font-bold text-white opacity-85">02</span>
                             </div>
                             <div class="flex flex-col items-center text-center text-white">
-                                <span>BUY</span>
-                                <p class="px-5">Pick Your Numbers & Complete your Purchase</p>
+                                <span>COMPRAR</span>
+                                <p class="px-5">Escolha seus números e conclua sua compra</p>
                             </div>
                         </div>
-                        <div class="bg-gradient-to-r from-violet-600 via-violet-600 to-indigo-600 flex min-h-80 flex-col items-center space-y-5 rounded-md p-2 py-5">
+                        <div class="flex min-h-80 flex-col items-center space-y-5 rounded-md bg-gradient-to-r from-violet-600 via-violet-600 to-indigo-600 p-2 py-5">
                             <div class="relative mx-auto flex h-36 w-36 items-center justify-center rounded-full border"><img src="https://pixner.net/rifa1/demo/assets/images/icon/play/3.png" alt="" class="rounded-full p-6 shadow-inner shadow-purple-600 hover:shadow-lg" /> <span class="absolute bottom-5 right-0 flex h-8 w-8 items-center justify-center rounded-full bg-purple-500 text-lg font-bold text-white opacity-85">03</span></div>
                             <div class="flex flex-col items-center text-center text-white">
-                                <span>WIN</span>
-                                <p class="px-5">Start Dreaming, you're almost there</p>
+                                <span>GANHAR</span>
+                                <p class="px-5">Comece a sonhar, você está quase lá</p>
                             </div>
                         </div>
                     </div>
@@ -70,26 +70,26 @@
             <div class="my-16 flex w-full">
                 <div class="flex w-full flex-col justify-center">
                     <div class="flex w-full flex-col items-center justify-center">
-                        <p class="text-lg text-yellow-400">Try your chance at winning</p>
-                        <h2 class="py-4 text-4xl font-bold text-slate-700 md:text-6xl  text-center">CURRENT CONTEST</h2>
-                        <p class="font-sans text-slate-500 text-center ">Participants buy tickets and lots are drawn to determine the winners.</p>
+                        <p class="text-lg text-yellow-400">Tente sua chance de ganhar</p>
+                        <h2 class="py-4 text-center text-4xl font-bold text-slate-700 md:text-6xl">RIFA ATUAL</h2>
+                        <p class="text-center font-sans text-slate-500">Os participantes compram ingressos e são sorteados lotes para determinar os vencedores.</p>
                         <div class="mt-10 flex w-full items-center justify-center space-x-5">
-                            <button type="button" class="md:w-52 rounded-full bg-gradient-to-t from-purple-500 to-yellow-300 py-3 px-2 text-white">
-                                <span> DREAM CAR </span>
+                            <button type="button" class="rounded-full bg-gradient-to-t from-purple-500 to-yellow-300 px-2 py-3 text-white md:w-52">
+                                <span>CARRO DOS DONHOS</span>
                             </button>
-                            <button type="button" class="md:w-52 rounded-full bg-gradient-to-l from-purple-700 to-purple-400 py-3 px-2 text-white">
-                                <span> ALL LIFESTYLE </span>
+                            <button type="button" class="rounded-full bg-gradient-to-l from-purple-700 to-purple-400 px-2 py-3 text-white md:w-52">
+                                <span>ESTILO DE VIDA</span>
                             </button>
                         </div>
                     </div>
                     <div class="mt-10 grid grid-cols-1 gap-3 rounded p-4 text-center md:grid-cols-3">
                         <div class="flex flex-col items-center bg-indigo-500 shadow-inner">
-                            <div class="mb-5 flex w-full cursor-pointer items-center justify-center bg-indigo-400 min-h-52">
+                            <a href="" class="mb-5 flex h-52 w-full cursor-pointer items-center justify-center bg-indigo-400">
                                 <img src="https://pixner.net/rifa1/demo/assets/images/contest/1.png" alt="" class="object-cover p-6 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110" />
-                            </div>
+                            </a>
                             <div class="relative flex w-full flex-col items-center">
                                 <div class="absolute -top-[50%] flex h-24 w-24 flex-col items-center justify-center rounded-full bg-pink-600 shadow-lg">
-                                    <p class="text-xs text-white">Contest Now</p>
+                                    <p class="text-xs text-white">Rifa agora</p>
                                     <span class="text-lg font-bold text-white">X9U</span>
                                 </div>
                                 <div class="mb-4 mt-4 grid w-full grid-cols-2 text-white">
@@ -99,10 +99,10 @@
                                     </div>
                                     <div class="mr-2 flex flex-col items-end">
                                         <span class="font-serif text-2xl font-bold text-green-500">R$ 9,99 </span>
-                                        <span> Trabilblazer </span>
+                                        <span> Preço por número </span>
                                     </div>
                                 </div>
-                                <div class="flex h-14 w-full items-center justify-around border-t border-indigo-400">
+                                <a href="" class="flex h-14 w-full items-center justify-around border-t border-indigo-400">
                                     <div class="flex space-x-3 text-white">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6 w-6">
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
@@ -114,18 +114,18 @@
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6 w-6">
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 6v.75m0 3v.75m0 3v.75m0 3V18m-9-5.25h5.25M7.5 15h3M3.375 5.25c-.621 0-1.125.504-1.125 1.125v3.026a2.999 2.999 0 0 1 0 5.198v3.026c0 .621.504 1.125 1.125 1.125h17.25c.621 0 1.125-.504 1.125-1.125v-3.026a2.999 2.999 0 0 1 0-5.198V6.375c0-.621-.504-1.125-1.125-1.125H3.375Z" />
                                         </svg>
-                                        <span> 9805 Remaining </span>
+                                        <span> 9805 Restante </span>
                                     </div>
-                                </div>
+                                </a>
                             </div>
                         </div>
                         <div class="flex flex-col items-center bg-indigo-500 shadow-inner">
-                            <div class="mb-5 flex w-full cursor-pointer items-center justify-center bg-indigo-400 min-h-52">
+                            <a href="" class="mb-5 flex h-52 w-full cursor-pointer items-center justify-center bg-indigo-400">
                                 <img src="https://pixner.net/rifa1/demo/assets/images/contest/3.png" alt="" class="object-cover p-6 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110" />
-                            </div>
+                            </a>
                             <div class="relative flex w-full flex-col items-center">
                                 <div class="absolute -top-[50%] flex h-24 w-24 flex-col items-center justify-center rounded-full bg-pink-600 shadow-lg">
-                                    <p class="text-xs text-white">Contest Now</p>
+                                    <p class="text-xs text-white">Rifa agora</p>
                                     <span class="text-lg font-bold text-white">X9U</span>
                                 </div>
                                 <div class="mb-4 mt-4 grid w-full grid-cols-2 text-white">
@@ -135,10 +135,10 @@
                                     </div>
                                     <div class="mr-2 flex flex-col items-end">
                                         <span class="font-serif text-2xl font-bold text-green-500">R$ 9,99 </span>
-                                        <span> Trabilblazer </span>
+                                        <span> Preço por número </span>
                                     </div>
                                 </div>
-                                <div class="flex h-14 w-full items-center justify-around border-t border-indigo-400">
+                                <a href="" class="flex h-14 w-full items-center justify-around border-t border-indigo-400">
                                     <div class="flex space-x-3 text-white">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6 w-6">
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
@@ -150,18 +150,18 @@
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6 w-6">
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 6v.75m0 3v.75m0 3v.75m0 3V18m-9-5.25h5.25M7.5 15h3M3.375 5.25c-.621 0-1.125.504-1.125 1.125v3.026a2.999 2.999 0 0 1 0 5.198v3.026c0 .621.504 1.125 1.125 1.125h17.25c.621 0 1.125-.504 1.125-1.125v-3.026a2.999 2.999 0 0 1 0-5.198V6.375c0-.621-.504-1.125-1.125-1.125H3.375Z" />
                                         </svg>
-                                        <span> 9805 Remaining </span>
+                                        <span> 9805 Restante </span>
                                     </div>
-                                </div>
+                                </a>
                             </div>
                         </div>
                         <div class="flex flex-col items-center bg-indigo-500 shadow-inner">
-                            <div class="mb-5 flex w-full cursor-pointer items-center justify-center bg-indigo-400 min-h-52">
+                            <a href="" class="mb-5 flex h-52 w-full cursor-pointer items-center justify-center bg-indigo-400">
                                 <img src="https://pixner.net/rifa1/demo/assets/images/contest/2.png" alt="" class="object-cover p-6 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110" />
-                            </div>
+                            </a>
                             <div class="relative flex w-full flex-col items-center">
                                 <div class="absolute -top-[50%] flex h-24 w-24 flex-col items-center justify-center rounded-full bg-pink-600 shadow-lg">
-                                    <p class="text-xs text-white">Contest Now</p>
+                                    <p class="text-xs text-white">Rifa agora</p>
                                     <span class="text-lg font-bold text-white">X9U</span>
                                 </div>
                                 <div class="mb-4 mt-4 grid w-full grid-cols-2 text-white">
@@ -171,10 +171,10 @@
                                     </div>
                                     <div class="mr-2 flex flex-col items-end">
                                         <span class="font-serif text-2xl font-bold text-green-500">R$ 9,99 </span>
-                                        <span> Trabilblazer </span>
+                                        <span> Preço por número </span>
                                     </div>
                                 </div>
-                                <div class="flex h-14 w-full items-center justify-around border-t border-indigo-400">
+                                <a href="" class="flex h-14 w-full items-center justify-around border-t border-indigo-400">
                                     <div class="flex space-x-3 text-white">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6 w-6">
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
@@ -186,9 +186,9 @@
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6 w-6">
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 6v.75m0 3v.75m0 3v.75m0 3V18m-9-5.25h5.25M7.5 15h3M3.375 5.25c-.621 0-1.125.504-1.125 1.125v3.026a2.999 2.999 0 0 1 0 5.198v3.026c0 .621.504 1.125 1.125 1.125h17.25c.621 0 1.125-.504 1.125-1.125v-3.026a2.999 2.999 0 0 1 0-5.198V6.375c0-.621-.504-1.125-1.125-1.125H3.375Z" />
                                         </svg>
-                                        <span> 9805 Remaining </span>
+                                        <span> 9805 Restante </span>
                                     </div>
-                                </div>
+                                </a>
                             </div>
                         </div>
                     </div>
