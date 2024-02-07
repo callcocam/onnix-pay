2ff4b57db0a258ff1ccdf9546e3dee4837d07902
Claudio Campos <callcocam@gmail.com>
2024-02-07 14:57:44 -0300

    rifa

--- resources/views/livewire/cart.blade.php
+++ resources/views/livewire/cart.blade.php
@@ -1,35 +1,37 @@
 <x-filament::modal id="cart" :slideOver="true">
     <x-slot name="heading">Carrinho</x-slot>
     <div>
-        @if( $this->cartItems && $this->cartItems->count())
+        @if($this->cartItems )
         <div class="flex-1 px-4 py-6 overflow-y-auto sm:px-6">
             <div class="flow-root">
                 <ul role="list" class="-my-6 divide-y divide-gray-200">
                     @foreach ($this->cartItems as $item)
-                    <li class="flex py-3">
-                        <div class="flex-shrink-0 w-20 h-20 overflow-hidden border border-gray-200 rounded-md">
-                            <img src="{{ $item->rifa->image_url }}" alt="{{ $item->rifa->name }}" class="object-cover object-center w-full h-full">
-                        </div>
-
-                        <div class="flex flex-col flex-1 ml-4">
-                            <div>
-                                <div class="flex justify-between text-base font-medium text-gray-900 dark:text-white">
-                                    <h3>
-                                        <a href="{{ route('rifas.show', ['record'=>$item->rifa]) }}">{{ $item->rifa->name }}</a>
-                                    </h3>
-                                    <p class="ml-4">R$ {{ $item->rifa->price  }}</p>
+                        @if($rifa = $item->rifa)
+                            <li class="flex py-3">
+                                <div class="flex-shrink-0 w-20 h-20 overflow-hidden border border-gray-200 rounded-md">
+                                    <img src="{{ $rifa->image_url }}" alt="{{ $rifa->name }}" class="object-cover object-center w-full h-full">
                                 </div>
-                                <p class="mt-1 text-sm text-gray-500 dark:text-gray-300">{{ $item->rifa->category->name  }}</p>
-                            </div>
-                            <div class="flex items-end justify-between flex-1 text-sm">
-                                <p class="text-gray-500 dark:text-gray-300">Quantidade {{ $item->numbers->count()}}</p>
 
-                                <div class="flex">
-                                    <button wire:click="removeItem('{{ $item->id }}')" type="button" class="font-medium text-primary-600 hover:text-primary-500">Remove</button>
+                                <div class="flex flex-col flex-1 ml-4">
+                                    <div>
+                                        <div class="flex justify-between text-base font-medium text-gray-900 dark:text-white">
+                                            <h3>
+                                                <a href="{{ route('rifas.show', ['record'=>$rifa]) }}">{{ $rifa->name }}</a>
+                                            </h3>
+                                            <p class="ml-4">R$ {{ $rifa->price  }}</p>
+                                        </div>
+                                        <p class="mt-1 text-sm text-gray-500 dark:text-gray-300">{{ $rifa->category->name  }}</p>
+                                    </div>
+                                    <div class="flex items-end justify-between flex-1 text-sm">
+                                        <p class="text-gray-500 dark:text-gray-300">Quantidade {{ $item->numbers->count()}}</p>
+
+                                        <div class="flex">
+                                            <button wire:click="removeItem('{{ $item->id }}')" type="button" class="font-medium text-primary-600 hover:text-primary-500">Remove</button>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
-                        </div>
-                    </li>
+                            </li>
+                        @endif
                     @endforeach
                 </ul>
             </div>
