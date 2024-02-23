7c3c75e4c3747c52872fee2fa299fe694d8ca262
Claudio Campos <callcocam@gmail.com>
2024-02-23 18:45:21 -0300

    ÚLTIMOS VENCEDORES

--- resources/views/livewire/rifas/rifas-component.blade.php
+++ resources/views/livewire/rifas/rifas-component.blade.php
@@ -1,7 +1,7 @@
  <div class="mt-10 grid grid-cols-1 gap-3 rounded p-4 text-center md:grid-cols-3">
     @if($rifas = $this->rifas)
-    @foreach($rifas as $rifa)
-    <livewire:rifas.rifa-component :rifa="$rifa" :key="$rifa->id" />
-    @endforeach
+        @foreach($rifas as $rifa)
+        <livewire:rifas.rifa-component :rifa="$rifa" :key="$rifa->id" />
+        @endforeach
     @endif
 </div>
