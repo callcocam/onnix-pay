cad961d203433bf699d09446eacd3e238a45c557
Claudio Campos <callcocam@gmail.com>
2024-03-08 13:22:47 -0300

    update

--- app/Console/Commands/UpdateOrderCommand.php
+++ app/Console/Commands/UpdateOrderCommand.php
@@ -30,9 +30,11 @@ class UpdateOrderCommand extends Command
         $sales = Sale::query()->whereIn('status', ['draft', 'pending', 'processing'])->get();
         if ($sales->count() > 0) {
             foreach ($sales as $sale) {
-                if ($invoice = data_get($sale, 'dataIvoice')) {
+                $invoce = json_decode($sale->data, true);
+                if ($invoice = data_get($invoce, 'invoice')) {
                     $data = Invoice::make()->ref(data_get($invoice, 'reference'));
                     if ($data) {
+                        $results[] = $data;
                         $sale->status = strtolower(data_get($data, 'invoice.status', 'pending'));
                         $sale->save();
                         $sale->numbers->each(function ($number) use ($data) {
